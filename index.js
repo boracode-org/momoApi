517a970b2314ca2796a0558e04e41dc27e20eb02
Stuart Dambi <stuartdambi@gmail.com>
2019-11-15 20:23:28 +0300

    added momo request to pay, getAccountBalance and transaction

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const momo = require('mtn-momo')
+require('dotenv').config()
+const http = require('http')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// primarykey: 02ff6612bf3c410e99df698658986c1d
-// Momo Sandbox Credentials {
-//     "userSecret": "50c05f2ef3014b69b6a986c5cd3c3fbd",
-//     "userId": "024284fa-77e3-4694-bc08-91c0313f4ab9"
-//   }
-
+// Setup Sandbox User
 const { Collections, Disbursements } = momo.create({
-    callbackHost: process.env.CALLBACK_HOST
+    callbackHost: process.env.HOST
 });
 
 const collections = Collections({
@@ -20,3 +17,38 @@ const collections = Collections({
     userId: process.env.USER_ID,
     primaryKey: process.env.PRIMARY_KEY
 });
+
+
+// Make request to pay
+collections
+    .requestToPay({
+        amount: '50',
+        currency: 'EUR',
+        externalId: '123456',
+        payer: {
+            partyIdType: "MSISDN",
+            partyId: "256774290781"
+        },
+        payerMessage: 'Am running a test',
+        payeeNote: 'Hello, are you working'
+    })
+    .then(transactionId => {
+        console.log({transactionId})
+        // Get status of transaction
+        return collections.getTransaction(transactionId)
+    })
+    .then(transaction => {
+        console.log({transaction})
+
+        // Get account balance
+        return collections.getBalance()
+    })
+    .then(accountBalance => {
+        console.log(accountBalance)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+let port = process.env.USER_ID
+console.log(port)
